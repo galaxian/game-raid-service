17bf9b2abb7c1bd0735faf7f724ef5213dfd1af7
Lee <94841127+galaxian@users.noreply.github.com>
2022-09-18 21:11:26 +0900

    feat(setting) : first setting

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as config from 'config';
+import { VersioningType } from '@nestjs/common';
+import { setupSwagger } from './config/swagger.config';
+
+const serverConfig = config.get('server');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = serverConfig.port;
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+    prefix: 'v',
+    defaultVersion: '1',
+  });
+
+  app.setGlobalPrefix('/api');
+
+  setupSwagger(app);
+  await app.listen(port);
 }
 bootstrap();
