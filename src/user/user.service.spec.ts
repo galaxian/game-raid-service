f2b01f1311f8dcfb62b58b1a837bd2dcb3a43b1f
Lee <94841127+galaxian@users.noreply.github.com>
2022-09-21 13:32:19 +0900

    test(user) : user 레이드 기록 조회 성공 테스트 코드 작성

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -54,4 +54,62 @@ describe('UserService', () => {
       expect(result.userId).toEqual(1);
     });
   });
+
+  describe('getUser', () => {
+    it('사용자 레이드 정보 조회 성공', async () => {
+      //given
+      const records: RaidRecord[] = [
+        {
+          id: 1,
+          score: 10,
+          level: 1,
+          enterTime: undefined,
+          endTime: undefined,
+          user: undefined,
+          deleteAt: undefined,
+        },
+        {
+          id: 2,
+          score: 20,
+          level: 2,
+          enterTime: undefined,
+          endTime: undefined,
+          user: undefined,
+          deleteAt: undefined,
+        },
+        {
+          id: 3,
+          score: 30,
+          level: 3,
+          enterTime: undefined,
+          endTime: undefined,
+          user: undefined,
+          deleteAt: undefined,
+        },
+      ];
+
+      mockUserRepository.findOne.mockImplementation(() =>
+        Promise.resolve({
+          raidRecord: records,
+          id: 1,
+          createAt: undefined,
+          updateAt: undefined,
+          deleteAt: undefined,
+        }),
+      );
+
+      //when
+      const id = 1;
+      let expectedTotalScore = 0;
+      for (const element of records) {
+        expectedTotalScore += element.score;
+      }
+      const result = await userService.getUser(id);
+
+      //then
+      expect(result.totalScore).toEqual(expectedTotalScore);
+      expect(result.bossRaidHistory[0].raidRecordId).toEqual(records[0].id);
+      expect(result.bossRaidHistory[0].score).toEqual(records[0].score);
+    });
+  });
 });
