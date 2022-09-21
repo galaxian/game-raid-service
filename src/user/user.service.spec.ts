b575c382c64e05d1919cf00efcbbebda9a1d7003
Lee <94841127+galaxian@users.noreply.github.com>
2022-09-21 13:31:21 +0900

    test(user) : user 생성 테스트 코드 작성

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
+import { RaidRecord } from 'src/raid/entity/raid.entity';
 import { User } from './entity/user.entity';
 import { UserService } from './user.service';
 
@@ -28,5 +29,29 @@ describe('UserService', () => {
 
   it('should be defined', () => {
     expect(userService).toBeDefined();
+  });
+
+  describe('createUser', () => {
+    it('사용자 생성 후 user pk.', async () => {
+      //given
+      mockUserRepository.create.mockImplementation(() => ({
+        id: undefined,
+        createDate: undefined,
+        updateDate: undefined,
+        deleteAt: undefined,
+        raidRecord: undefined,
+      }));
+      mockUserRepository.save.mockImplementation(() =>
+        Promise.resolve({
+          id: 1,
+        }),
+      );
+
+      //when
+      const result = await userService.createUser();
+
+      //then
+      expect(result.userId).toEqual(1);
+    });
   });
 });
